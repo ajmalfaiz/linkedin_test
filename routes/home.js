d23e2cfcfdb22fd30bcb28047c997739dbb47656
Ajmal Faiz Ummer <ajmal.ummer@fingent.com>
2023-06-19 23:10:07 +0530

    a

--- routes/home.js
+++ routes/home.js
@@ -2,18 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
-  console.log(req?.body)
-  const data = await fetch(req.body?.url, {
-    method: "POST",
-    headers: {
-      Accept: "application.json",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
-  return res.status(200).json({
-    returnUrl: data,
-    message: "this is the data",
-  });
+  console.log(req?.body);
+  try {
+    const response = await fetch(req.body?.url, {
+      method: "POST",
+      headers: {
+        Accept: "application.json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    const data = await response.json();
+    return res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(400).json({ error: "An error occurred" });
+  }
 });
 
 module.exports = router;
