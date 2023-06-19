f7d3fe3b895ecfc70e4f95e039e2b122615e14ac
Ajmal Faiz Ummer <ajmal.ummer@fingent.com>
2023-06-19 23:21:22 +0530

    fix

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const router = express.Router();
 
 router.post("/", async (req, res, next) => {
@@ -12,9 +12,27 @@ router.post("/", async (req, res, next) => {
         "Content-Type": "application/x-www-form-urlencoded",
       },
     });
+    return response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(400).json({ error: error });
+  }
+});
 
+router.post("/test", async (req, res, next) => {
+  try {
+    const response = await fetch(
+      "https://disease.sh/v3/covid-19/historical/all?lastdays=all",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const data = await response.json();
-    return res.json(data);
+    res.send(data);
   } catch (error) {
     console.error("Error:", error);
     res.status(400).json({ error: error });
